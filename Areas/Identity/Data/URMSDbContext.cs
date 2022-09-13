f6fed5b43682032dc0aeb198e93a989c3b5dd4bd
LENOVO <LENOVO@DESKTOP-12AB2J5>
2022-09-14 05:19:05 +0600

    Add project files.

---
+++ Areas/Identity/Data/URMSDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using URMS.Areas.Identity.Data;
+
+namespace URMS.Data
+{
+    public class URMSDbContext : IdentityDbContext<URMSUser>
+    {
+        public URMSDbContext(DbContextOptions<URMSDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using URMS.Areas.Identity.Data;
+using URMS.Data;
+
+[assembly: HostingStartup(typeof(URMS.Areas.Identity.IdentityHostingStartup))]
+namespace URMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                //services.AddDbContext<URMSDbContext>(options =>
+                //    options.UseSqlServer(
+                //        context.Configuration.GetConnectionString("URMSDbContextConnection")));
+
+                services.AddDefaultIdentity<URMSUser>(options => 
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<URMSDbContext>();
+            });
+        }
+    }
+}
