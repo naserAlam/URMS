2a7cb38e4931987b031a72eb321e2406cd494154
Naser Abdullah Alam <naser.abdullah.cse@ulab.edu.bd>
2022-09-15 08:52:43 +0600

    Update Database

---
+++ Migrations/20220915024750_AddedModels.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace URMS.Migrations
+{
+    public partial class AddedModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GradeReportId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Complaints",
+                columns: table => new
+                {
+                    ComplaintId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(nullable: true),
+                    URMSUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Complaints", x => x.ComplaintId);
+                    table.ForeignKey(
+                        name: "FK_Complaints_AspNetUsers_URMSUserId",
+                        column: x => x.URMSUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CoordinatorName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GradeReports",
+                columns: table => new
+                {
+                    GradeReportId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Semester = table.Column<string>(nullable: true),
+                    URMSUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradeReports", x => x.GradeReportId);
+                    table.ForeignKey(
+                        name: "FK_GradeReports_AspNetUsers_URMSUserId",
+                        column: x => x.URMSUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    DepartmentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                    table.ForeignKey(
+                        name: "FK_Courses_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Evaluations",
+                columns: table => new
+                {
+                    EvaluationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Semester = table.Column<string>(nullable: true),
+                    Score = table.Column<int>(nullable: false),
+                    CourseId = table.Column<int>(nullable: false),
+                    URMSUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evaluations", x => x.EvaluationId);
+                    table.ForeignKey(
+                        name: "FK_Evaluations_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Evaluations_AspNetUsers_URMSUserId",
+                        column: x => x.URMSUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    RegistrationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Semester = table.Column<string>(nullable: true),
+                    URMSUserId = table.Column<string>(nullable: true),
+                    CourseId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.RegistrationId);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_AspNetUsers_URMSUserId",
+                        column: x => x.URMSUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    ResultId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grade = table.Column<string>(nullable: true),
+                    Mark = table.Column<int>(nullable: false),
+                    GradeReportId = table.Column<int>(nullable: false),
+                    CourseId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.ResultId);
+                    table.ForeignKey(
+                        name: "FK_Results_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_GradeReports_GradeReportId",
+                        column: x => x.GradeReportId,
+                        principalTable: "GradeReports",
+                        principalColumn: "GradeReportId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Complaints_URMSUserId",
+                table: "Complaints",
+                column: "URMSUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_DepartmentId",
+                table: "Courses",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluations_CourseId",
+                table: "Evaluations",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluations_URMSUserId",
+                table: "Evaluations",
+                column: "URMSUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradeReports_URMSUserId",
+                table: "GradeReports",
+                column: "URMSUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_CourseId",
+                table: "Registrations",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_URMSUserId",
+                table: "Registrations",
+                column: "URMSUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_CourseId",
+                table: "Results",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_GradeReportId",
+                table: "Results",
+                column: "GradeReportId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Complaints");
+
+            migrationBuilder.DropTable(
+                name: "Evaluations");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "GradeReports");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "GradeReportId",
+                table: "AspNetUsers");
+        }
+    }
+}
